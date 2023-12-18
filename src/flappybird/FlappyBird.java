32d2afa4470fc6deee698dd33da9620ff11d13d6
hamnakashif <hamnakashif397@gmail.com>
2023-12-18 20:44:30 +0500

    adding a renderer class

--- src/flappybird/FlappyBird.java
+++ src/flappybird/FlappyBird.java
@@ -1,6 +1,7 @@
 package flappybird;
 
 import java.awt.Font;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,12 +11,14 @@ public class FlappyBird {
 	JFrame frame = new JFrame();
 	JLabel label = new JLabel("Hello");
 	public static FlappyBird flappybird;
+	public Renderer renderer;
 	public FlappyBird(){
 		
 		label.setBounds(0,0,100,50);
 		label.setFont(new Font(null,Font.PLAIN,25));
 		
-		
+		renderer=new Renderer();
+		frame.add(renderer);
 		frame.add(label);
 		
 		
@@ -25,6 +28,7 @@ public class FlappyBird {
 		frame.setVisible(true);
 		
 	}
+	
 	public static void main(String[]args)
 	{
 		flappybird=new FlappyBird();
--- src/flappybird/Renderer.java
+++ src/flappybird/Renderer.java
@@ -1,5 +1,15 @@
 package flappybird;
-
-public class Renderer {
+import javax.swing.JPanel;
+import java.awt.Graphics;
+public class Renderer extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		FlappyBird.flappybird.repaint(g);
+	}
 
 }
