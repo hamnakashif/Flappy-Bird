91b31abbfdd65667f8c4d8aca6fb36f6f55d727d
hamnakashif <hamnakashif397@gmail.com>
2023-12-27 19:42:48 +0500

    score problem fix images added

--- src/flappybird/FlappyBird.java
+++ src/flappybird/FlappyBird.java
@@ -3,6 +3,7 @@ package flappybird;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,16 +14,18 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class FlappyBird implements ActionListener, MouseListener, KeyListener {
+public class FlappyBird extends JPanel implements ActionListener, MouseListener, KeyListener {
 
+
+	private static final long serialVersionUID = 1L;
 	public static FlappyBird flappybird;
 	public final int width = 900, height = 500;
-	// JLabel label = new JLabel("Hello");
-	// public static FlappyBird flappybird;
-	public Renderer renderer;
 	// object of how bird will look
 	public Rectangle bird; // size color and posititon in render class
 	public Rectangle base; // size color and posititon in render class
@@ -30,18 +33,24 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener {
 	public ArrayList<Rectangle> columns;// number of columns will be generated using the array list
 	public Random rand;
 	public int ticks, yMotion, score;// motion of the bird along y axis
-	public boolean gameOver, started = false,passedThroughColumn = false;
+	public boolean gameOver, started = false,passedThroughColumn = false, passedThroughCurrentColumn=false;;
+	
 
 	public FlappyBird() {
 		JFrame frame = new JFrame();
 		Timer timer = new Timer(20, this);
-
-		renderer = new Renderer();
 		rand = new Random();
 
-		frame.add(renderer);
-		frame.addMouseListener(this);
-		frame.addKeyListener(this);
+		
+		//listener adding 
+		this.addMouseListener(this);
+		this.addKeyListener(this);
+		this.setFocusable(true);
+		
+		//adding flappy bird panel
+		frame.add(this);
+		
+		this.requestFocusInWindow(); 
 		frame.setTitle("Flappy Bird");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(width, height);
@@ -58,7 +67,14 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener {
 		timer.start();
 
 	}
-
+	
+	// Override paintComponent in FlappyBird JPanel
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        repaint(g);
+    }
+    
 	public void addColumn(boolean start) {
 		int spaceCol = 240;
 		int widthCol = 80;
@@ -138,83 +154,122 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener {
 			// bird moving
 			bird.y += yMotion;
 			// now checking for bird collision
-			for (Rectangle column : columns) {
-				// for score adding,10 for to remain within the speed,when the column y is 0
-				
-				if (column.intersects(bird)) 
+			for (Rectangle column : columns)
+			{
+				if (column.y == 0 && bird.x + bird.width / 2 > column.x + column.width / 2 - 10 && bird.x + bird.width / 2 < column.x + column.width / 2 + 10)
+				{
+					score++;
+				}
+
+				if (column.intersects(bird))
 				{
 					gameOver = true;
-					if (bird.x <= column.x) 
+
+					if (bird.x <= column.x)
 					{
-						bird.x = column.x - bird.width;// if bird falls then stopped by the column
-					} 
-					else 
+						bird.x = column.x - bird.width;
+
+					}
+					else
 					{
-						// if bird hits upper or lower column then bird dies
-						if (column.y != 0) 
+						if (column.y != 0)
 						{
 							bird.y = column.y - bird.height;
-						} else if (bird.y < column.height) 
+						}
+						else if (bird.y < column.height)
 						{
 							bird.y = column.height;
 						}
 					}
 				}
-				if (column.y == 0 && bird.x + bird.width / 2 > column.x + column.width / 2 - 10
-						&& bird.width / 2 < column.x + column.width / 2 + 10) 
-				{
-					score++;
-				}
 			}
-			
-			if (bird.y > height - 120 || bird.y < 0) {
+
+			if (bird.y > height - 120 || bird.y < 0) 
+			{
 
 				gameOver = true;
 			}
-			if (bird.y + yMotion >= height - 120) {
+			if (bird.y + yMotion >= height - 120) 
+			{
 				bird.y = height - 120 - bird.height; // the bird will fall to the base not out of the screen
 				gameOver = true;
 			}
 		}
 		//score++;
-		renderer.repaint();
+		this.repaint();
 	}
 
 	public void repaint(Graphics g) {
-		// TODO Auto-generated method stub
-		g.setColor(Color.cyan);
-		g.fillRect(0, 0, width, height);
+		//System.out.println("Repainting");
+		ImageIcon backgroundImageIcon = new ImageIcon("D:\\Program Files\\Flappy Bird\\assets\\background.png");
+	    Image backgroundImage = backgroundImageIcon.getImage();
 
-		g.setColor(Color.orange);
-		g.fillRect(0, height - 120, width, 120);
+	    // Draw background image
+	    g.drawImage(backgroundImage, 0, 0, width, height, this);
 
-		g.setColor(Color.green);
-		g.fillRect(0, height - 120, width, 20);
+	 // Load base image
+	    ImageIcon baseImageIcon = new ImageIcon("D:\\Program Files\\Flappy Bird\\assets\\border.png");
+	    Image baseImage = baseImageIcon.getImage();
 
-		g.setColor(Color.red);
-		g.fillRect(bird.x, bird.y, bird.width, bird.height);
+	    // Draw base image
+	    g.drawImage(baseImage, 0, height - 120, width, 120, this);
+
+		
+		ImageIcon birdIcon = new ImageIcon("D:\\Program Files\\Flappy Bird\\assets\\bird.png");
+	    Image birdImage = birdIcon.getImage();
+	    // Draw the bird image
+	    g.drawImage(birdImage, bird.x, bird.y, this);
 
 		for (Rectangle column : columns) {
 			paintColumn(g, column);// in this column are being from the array list
 		}
-
-		// if the game is started then what happens
-		g.setColor(Color.black);
-		g.setFont(new Font("Arial", 1, 30));
+		
 		if (!started) {
 
-			g.drawString("Click to start!", 75, height / 2 - 50);
+			// Load the image for the "Click to start" message
+	        ImageIcon startMessageIcon = new ImageIcon("D:\\Program Files\\Flappy Bird\\assets\\start.png");
+	        Image startMessageImage = startMessageIcon.getImage();
+	        // Draw the "Click to start" image
+	        g.drawImage(startMessageImage, 125, height / 2 - 150,250,250, this);
 		}
 		// when the gameOver variable is true then printing a string.
 		g.setColor(Color.white);
 		g.setFont(new Font("Arial", 1, 80));
 		if (gameOver) {
 
-			g.drawString("Game Over", 200, height / 2 - 50);
+			// Set the rectangle coordinates and dimensions
+	        int rectX = width / 4; // Adjust as needed //225
+	        int rectY = height / 4; // Adjust as needed //125
+	        int rectWidth = width / 2; // Adjust as needed //450
+	        int rectHeight = height / 2; // Adjust as needed //250
+
+	        System.out.println(rectX);
+	        System.out.println(rectY);
+	        System.out.println(rectWidth);
+	        System.out.println(rectHeight);
+	        // Draw the rectangle with the specified color
+	        g.setColor(new Color(206, 198, 115)); // RGB values for #CEC673
+	        g.fillRect(rectX +20, rectY, rectWidth-50, rectHeight);
+	        
+		    ImageIcon gameOverIcon = new ImageIcon("D:\\Program Files\\Flappy Bird\\assets\\gameover.png");
+	        Image gameOverImage = gameOverIcon.getImage();
+	        g.drawImage(gameOverImage, rectX + 140, rectY +20, 180, 70, this);
+
+	        // Display the score on top of the game over image
+	        g.setColor(Color.BLACK);
+	        g.setFont(new Font("Arial", Font.BOLD, 40));
+	        String scoreText = "Score: " + score;
+	        int scoreTextWidth = g.getFontMetrics().stringWidth(scoreText);
+	        g.drawString(scoreText, rectX + scoreTextWidth -10, rectY + rectHeight - 90);
+	        
+	        g.setColor(Color.BLACK);
+	        g.setFont(new Font("Arial", Font.PLAIN, 20));
+	        String text = "Click anywhere to start";
+	        g.drawString(text, rectX + scoreTextWidth -30, rectY + rectHeight - 40);
 		}
 		// drawing the score on the screen.
 		if (!gameOver && started) {
-			g.drawString(String.valueOf(score), width / 2 - 20, 100);
+			g.drawString(String.valueOf(score), width / 2 - 10, 100);
 		}
 	}
 
@@ -226,6 +281,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
+		//System.out.println("Mouse clicked");
 		jump();
 	}
 
@@ -268,6 +324,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
+		//System.out.println("Key pressed: " + e.getKeyCode());
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			jump();
 		}
--- src/flappybird/MainScreen.java
+++ src/flappybird/MainScreen.java
@@ -1,45 +1,108 @@
 package flappybird;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.JButton;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
-public class MainScreen implements ActionListener{
-
+public class MainScreen extends JPanel implements ActionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	JFrame frame = new JFrame();
 	JButton game = new JButton("Start Game");
-	JButton help = new JButton("Help");
-	MainScreen(){
-		
-		//start game button layout
-		game.setBounds(300,270,100,40);
-		game.setFocusable(false);
-		game.addActionListener(this);
-		//help button layout
-		help.setBounds(490,270,100,40);
-		help.setFocusable(false);
-		help.addActionListener(this);
-		
-		//adding the buttons to the main screen 
-		frame.add(game);
-		frame.add(help);
-		
-		//setting the size of the console screen
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(900,500);
-		frame.setLayout(null);
-		frame.setVisible(true);
-	}
 	
-	//what will happen when you press the button
+	MainScreen() throws IOException{
+		try {
+	    BufferedImage head = ImageIO.read(new File("D:\\Program Files\\Flappy Bird\\assets\\Heading.png"));
+        BufferedImage image = ImageIO.read(new File("D:\\Program Files\\Flappy Bird\\assets\\background.png"));
+        BufferedImage scaledImage = scaleImage(image, 900, 500);
+
+        ImageIcon backgroundIcon = new ImageIcon(scaledImage);
+        ImageIcon headIcon = new ImageIcon(head);
+
+        JLabel label = new JLabel(backgroundIcon);
+        JLabel labelHead = new JLabel(headIcon);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(900, 500));
+
+        label.setBounds(0, 0, 900, 500);
+        labelHead.setBounds(0, 0, 200, 300);
+
+        layeredPane.add(label, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(labelHead, JLayeredPane.PALETTE_LAYER);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(layeredPane, BorderLayout.CENTER);
+
+        // Calculate the bounds for center top placement
+        int screenWidth = 900; // Adjust to your screen width
+        int imageWidth = headIcon.getIconWidth();
+        int imageHeight = headIcon.getIconHeight();
+        int labelXHead = (screenWidth - imageWidth) / 2;
+        int labelYHead = 20;
+        labelHead.setBounds(labelXHead, labelYHead, imageWidth, imageHeight);
+
+        // Optional: Adding a button to the main screen
+       
+        game.setBounds(380, 270, 140, 40);
+        game.setFont(new Font("Arial", Font.BOLD, 18));
+        Color buttonColor = new Color(0x18, 0x99, 0xA4);
+        game.setBackground(buttonColor);
+        game.addActionListener(this);
+
+      //setting the size of the console screen
+      		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      		frame.setSize(900,500);
+      		frame.setLocationRelativeTo(null);//center of the screen
+        layeredPane.add(game, JLayeredPane.PALETTE_LAYER);
+
+        frame.setVisible(true);
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+	}
+	private BufferedImage scaleImage(BufferedImage image,int targetWidth,int targetHeight) {
+		BufferedImage scaledImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_ARGB);
+		scaledImage.getGraphics().drawImage(image.getScaledInstance(targetWidth, targetHeight, java.awt.Image.SCALE_SMOOTH), 0, 0, null);
+		return scaledImage;
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getSource()== game) {
-			FlappyBird flappyBird = new FlappyBird();
+			showGameScreen();
 		}
-		
 	}
+	private void showGameScreen() {
+		frame.getContentPane().removeAll();
+        frame.dispose(); // Close the main screen
+        FlappyBird flappyBird = new FlappyBird();
+	}
+	public static void main(String[] args) {
+        try {
+            new MainScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+    
+	
+	//what will happen when you press the button
 }
